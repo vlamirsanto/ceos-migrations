efdb7eca8c2c3c968ab3e8f6975ab267fcf8c00c
Fellipe Bastos <fellipe.bastos@dotlib.com.br>
2020-05-03 19:14:48 -0300

    Adiciona seed fixo para usario Ceos

--- src/database/seeders/20200502140834-demo-user.js
+++ src/database/seeders/20200502140834-demo-user.js
@@ -20,6 +20,18 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date(),
         },
+        {
+          name: faker.name.firstName(),
+          lastname: faker.name.lastName(),
+          email: 'ceos@email.com',
+          password: await bcrypt.hash("q1w2e3r4", 8),
+          whatsapp: "+5521980610491",
+          state: faker.address.stateAbbr(),
+          city: faker.address.city(),
+          profile_image: faker.image.avatar(),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
       ],
       {}
     );
